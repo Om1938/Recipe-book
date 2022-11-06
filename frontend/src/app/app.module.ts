902f957d500a942ab472045f6a7e2f9db2f07aab
Prajwal Patil <prajwalpatilk@gmail.com>
2022-11-06 12:30:32 +0530

    added frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, NgbModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
